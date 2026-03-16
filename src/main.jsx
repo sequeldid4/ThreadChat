93f96854d9ff73bfab02b66a42be1c110c955a29
sequeldid4 <nikhatyasmeen4322@gmail.com>
2026-03-16 11:02:43 +0530

    fix corrupted main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-cat > ~/thread/src/main.jsx << 'EOF'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
@@ -23,4 +22,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 )
-EOF
